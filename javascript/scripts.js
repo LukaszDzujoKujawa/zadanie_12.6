2480bd1a24d6d5bf3f667220db6094967f7b719d
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-25 22:11:41 +0200

    add files, structure, task

---
+++ javascript/scripts.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  var url = "https://restcountries.eu/rest/v2/name/";
+  var countriesList = $('#countries');
+
+  $('#search').click(searchCountries);
+  $('#country-name').keypress(function (e) {
+    if(e.which === 13) {
+      searchCountries();
+    }
+  });
+
+  function searchCountries() {
+    let countryName = $('#country-name').val();
+    if (!countryName.length) {
+      countryName = 'Poland';
+    }
+    $.getJSON(url + countryName, showCountriesList);
+  }
+
+  function showCountriesList(resp) {
+    countriesList.empty();
+    var $listItem = $('<li>').addClass('country').appendTo(countriesList);
+    var $colOne = $('<div>').addClass('col-one').appendTo($listItem);
+    var $colTwo = $('<div>').addClass('col-two').appendTo($listItem);
+    resp.forEach(function(item) {
+      $('<p>').text('Name').appendTo($colOne);
+      $('<p>').text('Flag').appendTo($colOne);
+      $('<p>').text('Population').appendTo($colOne);
+      $('<p>').text('Capital').appendTo($colOne);
+      $('<p>').text('Currency').appendTo($colOne);
+      $('<p>').text(item.name).appendTo($colTwo);
+      $('<figure>').addClass('figure-flag').appendTo($colTwo);
+      $('<img>').addClass('flag').attr('src', item.flag).appendTo('.figure-flag');
+      $('<p>').text(item.population).appendTo($colTwo);
+      $('<p>').text(item.capital).appendTo($colTwo);
+      $('<p>').text(item.currencies[0].code).appendTo($colTwo);
+    });
+  }
+});
